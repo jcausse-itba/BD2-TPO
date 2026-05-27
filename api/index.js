aadb73372ea8ac9227d4c6b471e5cfd0630a602e
nrivas-itba <138261486+nrivas-itba@users.noreply.github.com>
2026-05-26 23:05:54 -0300

    mongo connection
    Co-Authored-By: JaviertoZerrado <44640729+JaviertoZerrado@users.noreply.github.com>

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI);
 
 const app = express()
 
@@ -6,6 +9,11 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/a', (req, res) => {
+    const db = mongoose.connection.db;
+    res.send(db.listCollections().toArray())
 })
 
 app.listen(port, () => {
