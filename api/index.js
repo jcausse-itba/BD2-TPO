3a2e6d2480a096ee42645c9516d7b3d54dbe160e
nrivas-itba <138261486+nrivas-itba@users.noreply.github.com>
2026-05-26 23:24:20 -0300

    query1
    Co-Authored-By: JaviertoZerrado <44640729+JaviertoZerrado@users.noreply.github.com>

--- api/index.js
+++ api/index.js
@@ -11,9 +11,34 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/a', (req, res) => {
-    const db = mongoose.connection.db;
-    res.send(db.listCollections().toArray())
+app.get('/query1', async (req, res) => {
+    res.send(await mongoose.connection.db.collection('pacientes').aggregate([
+        {
+            $lookup: {
+                from: "propietarios",
+                localField: "id_propietario",
+                foreignField: "id_propietario",
+                as: "propietario"
+            }
+        },
+        { $unwind: "$propietario" },
+        {
+            $match: { activo: true }
+        },
+        {
+            $project: {
+                _id: 0,
+                nombre: 1,
+                "propietario.nombre": 1,
+                "propietario.apellido": 1,
+                "propietario.dni": 1,
+                "propietario.email": 1,
+                "propietario.telefono": 1,
+                "propietario.ciudad": 1,
+                "propietario.provincia": 1,
+            }
+        }
+    ]).toArray());
 })
 
 app.listen(port, () => {
